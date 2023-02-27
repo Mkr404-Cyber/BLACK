fbe68a1b9f8ca7780f7b7833e63cce720b90edab
Anonymous <115325635+Mkr404-Cyber@users.noreply.github.com>
2023-02-28 02:02:29 +0600

    Update MKR.py

--- MKR.py
+++ MKR.py
@@ -1,5 +1,5 @@
 #coding=utf-8
 import os, sys, time
-A = /x1b[1;97m
+A = '\x1b[1;91m'
 os.system('xdg-open https://crackbdofficial.blogspot.com');time.sleep(3)
 print(f'{A} Sorry Bro \n MKR OFLINE\n Good Bye ')
